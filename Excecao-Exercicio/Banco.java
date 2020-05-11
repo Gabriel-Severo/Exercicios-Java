4701a8c0487708be32d9ecef407a793394d8a389
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-11 18:43:04 -0300

    Add exercise 3

---
+++ Excecao-Exercicio/Banco.java
@@ -0,0 +1,12 @@
+public class Banco {
+    public static void main(String[] args) throws ContaException {
+        Conta minhaConta = new Conta();
+        minhaConta.deposita(100);
+        minhaConta.setLimite(100);
+        try{
+            minhaConta.saca(1000);
+        }catch(ContaException e){
+            System.out.println("O valor a ser sacado deve ser menor que o valor do saldo");
+        }
+    }
+}
---
+++ Excecao-Exercicio/Conta.java
@@ -0,0 +1,18 @@
+public class Conta {
+    private double saldo;
+    private double limite;
+    public void deposita(double saldo){
+        this.saldo += saldo;
+    }
+
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+
+    public void saca(double valor) throws ContaException{
+        if (valor > saldo){
+            throw new ContaException("O valor informado é maior do que o saldo");
+        }
+        this.saldo -= valor;
+    }
+}
