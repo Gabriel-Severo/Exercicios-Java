3d2097c8c9aa899169bf7da739adc87a796e8cf9
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-23 14:25:15 -0300

    Add Data Validator

---
+++ AgregacaoComposicao/ExercicioComposicao/Data.java
@@ -0,0 +1,49 @@
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data (int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String toString() {
+        boolean invalido = false;
+        if (dia > 31 || dia < 1 || mes > 12 || mes < 1 || ano < 1 || dia > 29 && mes == 2) {
+            invalido = true;
+        }
+        else if(mes == 2 && dia == 29 && !(ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0)){
+            invalido = true;
+        }
+        else if(mes < 8 && mes % 2 == 0 && dia == 31 || mes > 9 && mes % 2 != 0 && dia == 31){
+            invalido = true;
+        }
+        return dia + "/" + mes + "/" + ano + (invalido ? "(data inválida!)." : "");
+    }
+}
---
+++ AgregacaoComposicao/ExercicioComposicao/DataTeste.java
@@ -0,0 +1,6 @@
+public class DataTeste {
+    public static void main(String[] args) {
+        Data data = new Data(31, 12, 1904);
+        System.out.println(data.toString());
+    }
+}
