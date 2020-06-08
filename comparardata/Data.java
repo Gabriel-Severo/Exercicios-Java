4a34791b2765a73cd70c4ea9066068bb73a764b8
gabriel-severo <gabriel.ivi@hotmail.com>
2020-06-08 18:05:11 -0400

    Add Data exercise

---
+++ comparardata/Data.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.util.Collections;
+public class Data {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.println("=-Inserindo uma pessoa-=");
+            System.out.print("Digite o nome da pessoa: ");
+            String nome = s.nextLine();
+            System.out.print("Digite o sexo (M/F): ");
+            char sexo = s.nextLine().toLowerCase().charAt(0);
+            
+            System.out.print("Digite o dia: ");
+            int dia = s.nextInt();
+            System.out.print("Digite o mês: ");
+            int mes = s.nextInt();
+            System.out.print("Digite o ano: ");
+            int ano = s.nextInt();
+            
+            System.out.print("Digite a hora: ");
+            int hora = s.nextInt();
+            System.out.print("Digite o minuto: ");
+            int minuto = s.nextInt();
+            LocalDateTime nascimento = LocalDateTime.of(ano, mes, dia, hora, minuto);
+            System.out.println(nascimento);
+            Pessoa p = new Pessoa(nome, sexo, nascimento);
+            pessoas.add(p);
+            s.nextLine();
+            System.out.println("Inserir mais uma pessoa (S/N): ");
+            char opcao = s.nextLine().toLowerCase().charAt(0);
+            if(opcao == 'n')
+                break;
+        }
+        Collections.sort(pessoas);
+        for(Pessoa pessoa: pessoas){
+            System.out.println(pessoa);
+        }
+    }
+    
+}
---
+++ comparardata/Pessoa.java
@@ -0,0 +1,61 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Pessoa implements Comparable<Pessoa>{
+    private String nome;
+    private char sexo;
+    private LocalDateTime nascimento;
+
+    public Pessoa(String nome, char sexo, LocalDateTime nascimento) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.nascimento = nascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public LocalDateTime getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDateTime nascimento) {
+        this.nascimento = nascimento;
+    }
+    
+    @Override
+    public String toString(){
+        Period comparacao = Period.between(nascimento.toLocalDate(), LocalDate.now());
+        return  nome + " " + comparacao.getYears() + " anos\n" +
+                (sexo == 'm' ? "Masculino\n" : "Feminino\n") +
+                nascimento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) + "\n";
+    }
+    
+    @Override
+    public int compareTo(Pessoa pessoa){
+        if(nascimento.isAfter(pessoa.nascimento)){
+            return -1;
+        }
+        if(nascimento.isBefore(pessoa.nascimento)){
+            return 1;
+        }
+        return 0;
+    }
+    
+    
+}
